5281500f8602f2d420d90fd038e2aa828d4fa3f9
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-11-09 00:03:17 -0500

    Added css and cleaned up some white space

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const express = require('express')
 const showdata = require('./showdata')
-
-
 const app = express()
 
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 app.get('/', (request, response) => {
-  console.log('lol')
-
   return response.render('index', { showdata })
 })
 
 app.get('/seasons/:id', (request, response) => {
-  // console.log(showdata.find((season) => season.id === parseInt(request.params.id)))
-  console.log(showdata.seasons[request.params.id - 1])
   const seasonAll = showdata.seasons[request.params.id - 1]
 
   return response.render('seasons', { seasonAll })
